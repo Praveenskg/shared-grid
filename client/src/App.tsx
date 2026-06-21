ca906b717013de1d5b3be99731fcf386870fcc0b
Praveen Singh <Praveencdg@gmail.com>
2026-06-22 01:19:35 +0530

    feat: implement realtime tile synchronization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,21 @@
 import { GridCanvas } from "@/components/grid-canvas";
+import { useEffect } from "react";
+import { socket } from "./lib/socket";
 
 function App() {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected:", socket.id);
+    });
+
+    socket.on("tile-updated", (tile) => {
+      console.log("Tile Updated:", tile);
+    });
+
+    return () => {
+      socket.off("tile-updated");
+    };
+  }, []);
   return (
     <main className="min-h-screen bg-slate-100 p-8">
       <div className="mx-auto max-w-7xl">
--- server/src/routes/tile-routes.ts
+++ server/src/routes/tile-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { Tile } from "../models/tile";
+import { getIO } from "../sockets/socket-instance";
 
 const router = Router();
 
@@ -14,33 +15,47 @@ router.get("/", async (_, res) => {
 });
 
 router.patch("/:tileId/claim", async (req, res) => {
-  const { tileId } = req.params;
+  try {
+    const { tileId } = req.params;
 
-  const {
-    ownerId,
-    ownerName,
-    color,
-  } = req.body;
+    const { ownerId, ownerName, color } = req.body;
 
-  const tile = await Tile.findOneAndUpdate(
-    {
-      tileId: Number(tileId),
-    },
-    {
-      ownerId,
-      ownerName,
-      color,
-      claimedAt: new Date(),
-    },
-    {
-      returnDocument: 'after'
-    },
-  );
+    const tile = await Tile.findOneAndUpdate(
+      {
+        tileId: Number(tileId),
+      },
+      {
+        ownerId,
+        ownerName,
+        color,
+        claimedAt: new Date(),
+      },
+      {
+        returnDocument: "after",
+      },
+    );
 
-  res.json({
-    success: true,
-    data: tile,
-  });
+    if (!tile) {
+      return res.status(404).json({
+        success: false,
+        message: "Tile not found",
+      });
+    }
+
+    getIO().emit("tile-updated", tile);
+
+    res.json({
+      success: true,
+      data: tile,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 });
 
 export default router;
